fe7590dab23111b3ab1164c198fe6dd8a1dbd9d9
Akhil Kumar <gargakhil90@gmail.com>
2025-07-25 18:36:17 +0530

    python connector_latest_sql_1

--- dev/02_stage_sales_data.sql
+++ dev/02_stage_sales_data.sql
@@ -1,4 +1,5 @@
 INSERT INTO sales_data_raw (sale_id, sale_date, amount, region)
 VALUES
     (1, '2023-01-01', 100, 'North'),
-    (2, '2023-01-02', 150, 'South');
+    (2, '2023-01-02', 150, 'South'),
+    (3, '2025-07-25', 200, 'North');
