8fed7b8b628f13196ba275a04201509690671611
Akhil Kumar <gargakhil90@gmail.com>
2025-07-25 12:15:54 +0530

    Fix 7

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -3,29 +3,29 @@ set -e
 
 echo "Deploying to Snowflake with snowsql..."
 
-~/bin/snowsql -a $SNOWFLAKE_ACCOUNT \
-  -u $USER \
-  -p $PASSWORD \
-  -r $ROLE \
-  -w $WAREHOUSE \
-  -d $DATABASE \
-  -s $SCHEMA \
+~/bin/snowsql -a "$SNOWFLAKE_ACCOUNT" \
+  -u "$USER" \
+  -p "$PASSWORD" \
+  -r "$ROLE" \
+  -w "$WAREHOUSE" \
+  -d "$DATABASE" \
+  -s "$SCHEMA" \
   -f dev/create_tables.sql
 
-~/bin/snowsql -a $SNOWFLAKE_ACCOUNT \
-  -u $USER \
-  -p $PASSWORD \
-  -r $ROLE \
-  -w $WAREHOUSE \
-  -d $DATABASE \
-  -s $SCHEMA \
+~/bin/snowsql -a "$SNOWFLAKE_ACCOUNT" \
+  -u "$USER" \
+  -p "$PASSWORD" \
+  -r "$ROLE" \
+  -w "$WAREHOUSE" \
+  -d "$DATABASE" \
+  -s "$SCHEMA" \
   -f dev/stage_sales_data.sql
 
-~/bin/snowsql -a $SNOWFLAKE_ACCOUNT \
-  -u $USER \
-  -p $PASSWORD \
-  -r $ROLE \
-  -w $WAREHOUSE \
-  -d $DATABASE \
-  -s $SCHEMA \
+~/bin/snowsql -a "$SNOWFLAKE_ACCOUNT" \
+  -u "$USER" \
+  -p "$PASSWORD" \
+  -r "$ROLE" \
+  -w "$WAREHOUSE" \
+  -d "$DATABASE" \
+  -s "$SCHEMA" \
   -f dev/transform_sales_data.sql
